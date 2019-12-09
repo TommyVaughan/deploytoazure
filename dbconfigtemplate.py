8814ca9d07c57f70b8b57c34d5dd53b595fd30d8
TommyVaughan <35727694+TommyVaughan@users.noreply.github.com>
2019-12-09 10:04:23 +0000

    Add files via upload

---
+++ dbconfigtemplate.py
@@ -0,0 +1,6 @@
+mysql={
+    'host': 'localhost',
+    'username': 'root',  #user="datarep"  
+    'password': '',  #passwd="password"
+    'database': "datarepresentation"
+}
---
+++ filmDAO.py
@@ -0,0 +1,72 @@
+import mysql.connector
+import dbconfig as cfg
+
+
+class FilmDAO:
+    db=""
+    def __init__(self): 
+        self.db = mysql.connector.connect(
+        host= cfg.mysql['host'],
+        user= cfg.mysql['user'],
+        password= cfg.mysql['password'],
+        database= cfg.mysql['database']
+        )
+    
+            
+    def create(self, values):
+        cursor = self.db.cursor()
+        sql="insert into film (title,director, released) values (%s,%s,%s)"
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        return cursor.lastrowid
+
+    def getAll(self):
+        cursor = self.db.cursor()
+        sql="select * from film"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        print(results)
+        for result in results:
+            print(result)
+            returnArray.append(self.convertToDictionary(result))
+
+        return returnArray
+
+    def findByID(self, id):
+        cursor = self.db.cursor()
+        sql="select * from film where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        return self.convertToDictionary(result)
+
+    def update(self, values):
+        cursor = self.db.cursor()
+        sql="update film set title= %s,director=%s, released=%s  where id = %s"
+        cursor.execute(sql, values)
+        self.db.commit()
+    def delete(self, id):
+        cursor = self.db.cursor()
+        sql="delete from film where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        print("delete done")
+
+    def convertToDictionary(self, result):
+        colnames=['id','Title','Director', "Released"]
+        item = {}
+        
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                item[colName] = value
+        
+        return item
+        
+filmDAO = FilmDAO()
